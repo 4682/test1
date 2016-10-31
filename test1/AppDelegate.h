9a9da891aff5c58ba445bfe4ef5662ab98b29af3
wangxun <46822192@qq.com>
2016-10-31 21:20:39 +0800

    Initial Commit

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by wangxun on 16/10/31.
+//  Copyright © 2016年 wangxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by wangxun on 16/10/31.
+//  Copyright © 2016年 wangxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
